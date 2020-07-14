94f7a845b77d33efa4eae916c8c8055b38bcc0df
Oleksandr Krasnoporov <alexbit@outlook.com>
2020-07-15 00:16:03 +0300

    add azure provider

--- connections.tf
+++ connections.tf
@@ -8,9 +8,20 @@ provider "aws" {
     region = "us-west-2"
 }
 
-provider "azurerm" {
-    subscription_id = "0"
-    client_id = "1"
-    client_secret= "2"
-    tenant_id = ""
+#AZURE
+/* provider "azurerm" {
+    version = "=2.18.0"
+    features {}
+    subscription_id = var.subscription_id
+    client_id = var.client_id
+    client_secret= var.client_secret
+    tenant_id = var.tenant_id
+    
 }
+
+variable subscription_id {}
+variable client_id {}
+variable client_secret {}
+variable tenant_id {} */
+
+
--- resources.tf
+++ resources.tf
@@ -1,6 +1,6 @@
 resource "google_compute_network" "our_development_network" {
     name = "devnetwork"
-    auto_create_subnetworks = true
+    auto_create_subnetworks = false
 }
 
 resource "aws_vpc" "environmnet-example-two" {
@@ -11,3 +11,57 @@ resource "aws_vpc" "environmnet-example-two" {
      Name = "terraform-aws-vpc-example-two"
     }
 }
+
+resource "aws_subnet" "subnet1" {
+    #cidr_block = aws_vpc.environmnet-example-two.id
+    cidr_block = cidrsubnet(aws_vpc.environmnet-example-two.cidr_block, 3, 1)
+    vpc_id = aws_vpc.environmnet-example-two.id
+    availability_zone = "us-west-2a"
+
+}
+
+resource "aws_subnet" "subnet2" {
+    #cidr_block = aws_vpc.environmnet-example-two.id
+    cidr_block = cidrsubnet(aws_vpc.environmnet-example-two.cidr_block, 2, 2)
+    vpc_id = aws_vpc.environmnet-example-two.id
+    availability_zone = "us-west-2b"
+
+}
+
+resource "aws_security_group" "subnetsecurity" {
+    vpc_id = aws_vpc.environmnet-example-two.id 
+    ingress {
+        cidr_blocks = [
+            aws_vpc.environmnet-example-two.cidr_block
+        ]
+
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+    }
+}
+
+
+#AZURE
+
+/* resource "azurerm_resource_group" "azy_network" {
+    location = "West US"
+    name = "devresgrp"
+}
+
+resource "azurerm_virtual_network" "blue_virtual_network" {
+    address_space = ["10.0.0.0/16"]
+    location = "West US"
+    name = "bluevirtnetwork"
+    resource_group_name = azurerm_resource_group.azy_network.name
+    dns_servers = ["10.0.0.4", "10.0.0.5"]
+
+    subnet {
+        name = "subnet1"
+        address_prefix = "10.0.1.0/24"
+    }
+
+    tags = {
+        environmnet = "blue-world-finder"
+    }
+} */
